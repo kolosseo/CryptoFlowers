6cff7bf05de1e79c5c3e13a15c90dd135cee1350
Jacopo Pace <my.root.access@gmail.com>
2017-08-19 03:17:19 +0200

    clean tabs on main.js

--- js/main.js
+++ js/main.js
@@ -34,116 +34,116 @@ var Ws = (function() {
 
     let api = {};
 
-	//var wsUri = 'ws://echo.websocket.org/';
-	api.wsUri = 'wss://ws.blockchain.info/inv';
-	api.websocket = null;
-	api.waitTx = false;
+    //var wsUri = 'ws://echo.websocket.org/';
+    api.wsUri = 'wss://ws.blockchain.info/inv';
+    api.websocket = null;
+    api.waitTx = false;
     api.isOpen = false;
-	api.tx = {
+    api.tx = {
         name: 'root',
         size: 100,
         children: []
     };
-	api.output = null;
-	api.pingTimeout = null;
-	api.getTxTimeout = null;
+    api.output = null;
+    api.pingTimeout = null;
+    api.getTxTimeout = null;
 
-	api.init = function() {
-		api.output = document.getElementById("output");
-		api.testWebSocket();
-	}
+    api.init = function() {
+        api.output = document.getElementById("output");
+        api.testWebSocket();
+    }
 
-	api.testWebSocket = function() {
-	    api.websocket = new WebSocket(api.wsUri);
-	    api.websocket.onopen = function(evt) { api.onOpen(evt) };
-	    api.websocket.onclose = function(evt) { api.onClose(evt) };
-	    api.websocket.onmessage = function(evt) { api.onMessage(evt) };
-	    api.websocket.onerror = function(evt) { api.onError(evt) };
-	}
+    api.testWebSocket = function() {
+        api.websocket = new WebSocket(api.wsUri);
+        api.websocket.onopen = function(evt) { api.onOpen(evt) };
+        api.websocket.onclose = function(evt) { api.onClose(evt) };
+        api.websocket.onmessage = function(evt) { api.onMessage(evt) };
+        api.websocket.onerror = function(evt) { api.onError(evt) };
+    }
 
-	api.onOpen = function(evt) {
-		console.log('CONNECTED');
+    api.onOpen = function(evt) {
+        console.log('CONNECTED');
         api.isOpen = true;
-		//doSend('{"op":"ping_block"}');
-		//doSend('{"op":"ping_tx"}');
-		//ping();
-		api.getTx();
-	}
+        //doSend('{"op":"ping_block"}');
+        //doSend('{"op":"ping_tx"}');
+        //ping();
+        api.getTx();
+    }
 
-	api.ping = function() {
-		api.pingTimeout = setTimeout(function() {
-			api.doSend('{"op":"ping"}');
-			api.ping();
-		}, 20000);
-	}
+    api.ping = function() {
+        api.pingTimeout = setTimeout(function() {
+            api.doSend('{"op":"ping"}');
+            api.ping();
+        }, 20000);
+    }
 
-	api.getTx = function() {
-		api.getTxTimeout = setTimeout(function() {
-			api.waitTx = true;
-			api.doSend('{"op":"ping_tx"}');
-			api.getTx();
-		}, 1000);
-	}
+    api.getTx = function() {
+        api.getTxTimeout = setTimeout(function() {
+            api.waitTx = true;
+            api.doSend('{"op":"ping_tx"}');
+            api.getTx();
+        }, 1000);
+    }
 
-	api.onClose = function(evt) {
-		console.log('DISCONNECTED');
+    api.onClose = function(evt) {
+        console.log('DISCONNECTED');
         api.isOpen = false;
-		//clearTimeout(pingTimeout);
-		clearTimeout(api.getTxTimeout);
-	}
+        //clearTimeout(pingTimeout);
+        clearTimeout(api.getTxTimeout);
+    }
 
-	api.onMessage = function(evt) {
-		var children = [];
-		var res = JSON.parse(evt.data);
-		const RATIO = 1000000;
-		console.log('RESPONSE:', res);
+    api.onMessage = function(evt) {
+        var children = [];
+        var res = JSON.parse(evt.data);
+        const RATIO = 1000000;
+        console.log('RESPONSE:', res);
 
-		if (api.waitTx && res.op === 'utx') {
-			api.waitTx = false;
+        if (api.waitTx && res.op === 'utx') {
+            api.waitTx = false;
             console.log('API.TX 1:', api.tx)
-			if ((api.tx.children.length === 0)
-			 || ((api.tx.children[0]) && (api.tx.children[0].hash !== res.x.hash))) {
+            if ((api.tx.children.length === 0)
+             || ((api.tx.children[0]) && (api.tx.children[0].hash !== res.x.hash))) {
 
-				for (var i = 0; i < res.x.inputs.length; i++) {
-					children.push({
-						name: res.x.inputs[i].prev_out.addr,
-						size: res.x.inputs[i].prev_out.value / RATIO
-					})
-				}
+                for (var i = 0; i < res.x.inputs.length; i++) {
+                    children.push({
+                        name: res.x.inputs[i].prev_out.addr,
+                        size: res.x.inputs[i].prev_out.value / RATIO
+                    })
+                }
 
                 // keep the app lighter ;)
                 if (api.tx.children.length > 30)
                     api.tx.children = [];
 
-				api.tx.children.unshift({
-					name: res.x.hash,
-					size: res.x.out[0].value / RATIO,
-					children: children
-				});
+                api.tx.children.unshift({
+                    name: res.x.hash,
+                    size: res.x.out[0].value / RATIO,
+                    children: children
+                });
 
                 console.log('API.TX 2:', api.tx)
 
-				currentCodeFlower.update(api.tx);
-			}
-			//else console.log('SAME TX');
-		}
-	}
+                currentCodeFlower.update(api.tx);
+            }
+            //else console.log('SAME TX');
+        }
+    }
 
-	api.onError = function(evt) {
-		console.log('ERROR:', evt.data);
-	}
+    api.onError = function(evt) {
+        console.log('ERROR:', evt.data);
+    }
 
-	api.doSend = function(message) {
-		console.log('SENT: ' + message);
-		api.websocket.send(message);
-	}
+    api.doSend = function(message) {
+        console.log('SENT: ' + message);
+        api.websocket.send(message);
+    }
 
-	api.writeToScreen = function(message) {
-		var pre = document.createElement('p');
-		pre.style.wordWrap = 'break-word';
-		pre.innerHTML = message;
-		api.output.appendChild(pre);
-	}
+    api.writeToScreen = function(message) {
+        var pre = document.createElement('p');
+        pre.style.wordWrap = 'break-word';
+        pre.innerHTML = message;
+        api.output.appendChild(pre);
+    }
 
     return api;
 
